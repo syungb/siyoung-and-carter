d21ea2fd92b3a9651b0c0eb2e92d4ab89d304956
Siyoung Byun <syungb@gmail.com>
2020-12-24 22:35:55 -0500

    remove out file

--- out/production/siyoung-and-carter/app/components/footer.cljs
+++
@@ -1,9 +0,0 @@
-(ns app.components.footer)
-
-(def copyright
-  "Copyright Ⓡ 2019 Siyoung Byun. All rights reserved.")
-
-(defn footer
-  []
-  [:footer
-   [:span copyright]])
--- out/production/siyoung-and-carter/app/pages/our_story.cljs
+++
@@ -1,6 +0,0 @@
-(ns app.pages.our-story)
-
-(defn photo-roll [photos]
-  (for [photo photos]
-    [:div.]))
-
