efb15b9cab46ea6e6c42727da79c0d75cf2c723b
Siyoung Byun <siyoung.byun@kirasystems.com>
2022-05-16 17:55:46 -0400

    update main photo and style

--- src/app/pages/home.cljs
+++ src/app/pages/home.cljs
@@ -1,13 +1,12 @@
 (ns app.pages.home
   (:require
-    [reagent.core :as r]))
+   [reagent.core :as r]))
 
 (defn main-photo
   []
   [:div.image_renderer
-   [:img {:class "image_renderer__img"
-          :src "/img/main_photo.jpg"
-          :alt "Carter and Siyoung in Yosemite 2019"}]
+   [:img {:class ["image_renderer__img" "main__photo"]
+          :src "/img/year_08.jpg"}]
    [:div.text__block
     [:h2.text-align-center.date "June 18th, 2022"]
     [:h2.text-align-center.date "2022년 6월 18일"]
